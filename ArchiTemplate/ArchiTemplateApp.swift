f41353131bbad607597bccfd1eaeb07343c45830
Adrien <adrnsmn@gmail.com>
2021-10-23 21:23:30 +1200

    Initial Commit

---
+++ ArchiTemplate/ArchiTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiTemplateApp.swift
+//  ArchiTemplate
+//
+//  Created by Admin on 23/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ArchiTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
